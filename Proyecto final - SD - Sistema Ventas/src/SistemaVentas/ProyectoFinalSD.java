a884a6d7b25cb9e3b4062cadf0fa4956783c0451
Braulio20182313 <bchavezn@unsa.edu.pe>
2022-07-07 19:50:43 -0500

    creación del proyecto, packages, imagenes y librerias 02

---
+++ Proyecto final - SD - Sistema Ventas/src/SistemaVentas/ProyectoFinalSD.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package SistemaVentas;
+
+public class ProyectoFinalSD {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola mundo");
+    }
+    
+}
